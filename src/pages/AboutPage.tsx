737c02e8c300ea6e74212907581251b4a06eb3c0
Olly <ollycodesdev@gmail.com>
2026-08-06 21:15:07 +0200

    feat: add about page content

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,19 @@
 const AboutPage = () => {
     return (
         <main>
-            <h1>This is the About Page</h1>
+            <header>
+                <h1>About Ronin <span>Log</span></h1>
+            </header>
+
+            <section>
+                <p>
+                    Ronin <span>Log</span> is a platform for writing and sharing reviews of anime, manga, light novels, and many more.
+                </p>
+                <p>
+                    Sign in to leave comments on any review, or browse without an account 
+                    to read through what's been reviewed so far.
+                </p>
+            </section>
         </main>
     );
 };
