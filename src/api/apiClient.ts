460aaa9bba4b672cc86a3c2ec88be608f7ec3a7c
Olly <ollycodesdev@gmail.com>
2026-08-06 02:50:51 +0200

    feat: configure api client

---
+++ src/api/apiClient.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:3001",
+    headers: {
+        "Content-Type": "application/json",
+    }
+});
+
+apiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+apiClient.interceptors.response.use(
+    (res) => res, (error) => {
+        const skip = error.config?.skipAuthRedirect;
+
+        if (error.response?.status === 401 && !skip) {
+            localStorage.removeItem("token");
+            window.location.href = "/auth/login";
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default apiClient;
