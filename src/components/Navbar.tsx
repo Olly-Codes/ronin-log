3bde61214d38136d6a2f8b55fa158ad0667e928d
Olly <ollycodesdev@gmail.com>
2026-08-06 21:45:17 +0200

    style: navigation links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,11 +8,12 @@ const Navbar = () => {
     ];
 
     return (
-        <nav>
+        <nav className="flex items-center gap-4">
             {links.map((link) => (
                 <NavLink 
                     key={link.to}
                     to={link.to}
+                    className={({ isActive }) => `py-2 px-2 ${isActive ? "border-b border-red-600" : "text-muted hover:text-accent"}`}
                 >
                     {link.label}
                 </NavLink>
