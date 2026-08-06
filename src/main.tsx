e7c5a5170dee23e0280e31f840755739c4e5ef78
Olly <ollycodesdev@gmail.com>
2026-08-06 03:59:33 +0200

    feat: add react toaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { AuthProvider } from './context/AuthContext.tsx';
+import { Toaster } from 'react-hot-toast';
 import App from './App.tsx'
+
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
+      <Toaster />
       <App />
     </AuthProvider>
   </StrictMode>,
