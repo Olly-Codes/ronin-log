93d7514d648270e1c7339c392604e808ef529b31
Olly <ollycodesdev@gmail.com>
2026-08-06 21:38:02 +0200

    style: styling header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,13 +10,33 @@ const Layout = () => {
 
     return (
         <>
-            <header>
-                <p>Ronin <span>Log</span></p>
+            <header className="h-16 bg-surface border-b border-border flex items-center justify-around text-primary font-bold">
+                <p>Ronin <span className="text-accent">Log</span></p>
                 <Navbar />
-                {user ? (<button onClick={logout}>Logout</button>) : (
-                    <div>
-                        <button onClick={() => navigate("/auth/login")}>Login</button>
-                        <button onClick={() => navigate("/auth/register")}>Register</button>
+                {user ? (
+                    <div className="flex items-center gap-4">
+                        <p>{user.username}</p>
+                        <button 
+                            onClick={logout}
+                            className="px-2 cursor-pointer bg-red-600 py-2 hover:bg-accent-hover disabled:opacity-50 disabled:cursor-not-allowed transition-colors duration-300"
+                        >
+                            Logout
+                        </button>
+                    </div>
+                ) : (
+                    <div className="flex items-center gap-4">
+                        <button 
+                            onClick={() => navigate("/auth/login")}
+                            className="px-2 cursor-pointer bg-red-600 py-2 hover:bg-accent-hover disabled:opacity-50 disabled:cursor-not-allowed transition-colors duration-300"
+                        >
+                            Login
+                        </button>
+                        <button 
+                            onClick={() => navigate("/auth/register")}
+                            className="px-2 cursor-pointer border border-text-muted py-2 hover:bg-gray-600 disabled:opacity-50 disabled:cursor-not-allowed transition-colors duration-300"
+                        >
+                            Register
+                        </button>
                     </div>
                 )}
             </header>
