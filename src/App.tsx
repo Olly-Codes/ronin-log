1f20484ba951c92f08dc9487f0b3caf3586f535a
Olly <ollycodesdev@gmail.com>
2026-08-06 02:42:42 +0200

    feat: add page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import ReviewsPage from "./pages/ReviewsPage";
+import AboutPage from "./pages/AboutPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
 function App() {
 
   return (
-    <>
-      <h1>This is the home page</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/reviews" element={<ReviewsPage />} />
+        </Route>
+        <Route path="/auth/login" element={<LoginPage />} />
+        <Route path="/auth/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
