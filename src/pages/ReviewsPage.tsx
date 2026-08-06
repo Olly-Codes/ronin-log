5c7b1ec5cbd3c91a44d65946403183c4f1e7b346
Olly <ollycodesdev@gmail.com>
2026-08-06 19:17:32 +0200

    feat: add rendering of reviews

--- src/pages/ReviewsPage.tsx
+++ src/pages/ReviewsPage.tsx
@@ -1,7 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
+import reviewsAPI from "../api/reviewsAPI";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router";
+
 const ReviewsPage = () => {
+
+    const navigate = useNavigate();
+    const reviewsQuery = useQuery({
+        queryKey: ["reviews"],
+        queryFn: reviewsAPI.getReviews
+
+    });
+
+    if (reviewsQuery.isLoading) return <p>Loading Reviews...</p>
+    if (reviewsQuery.isError) {
+        toast.error("Could not load reviews. Please try again");
+    }
+    
     return (
         <main>
-            <h1>This is the Reviews Page</h1>
+            <h1>Reviews</h1>
+            {reviewsQuery.data ? (reviewsQuery.data.reviews.map((review) => (
+                <article
+                    key={review.review_id}
+                    onClick={() => navigate(`${review.review_id}`)}
+                >
+                    <section>
+                        {review.cover_image_url ? (
+                            <img 
+                                src={review.cover_image_url}
+                                alt={review.title}
+                            />
+                        ) : (
+                            <div>
+                                No image
+                            </div>
+                        )}
+                    </section>
+
+                    <section>
+                        <p>{review.media_type} &bull; {review.demographic}</p>
+                        <div>
+                            <h2>{review.title}</h2>
+                            <span>{review.score}</span>
+                        </div>
+                        <div>
+                            {review.genres.map((genre) => (
+                                <span
+                                    key={genre}>
+                                        {genre}
+                                    </span>
+                            ))}
+                        </div>
+                    </section>
+                </article>
+            ))
+            ) : (
+                <p>Failed to load reviews</p>
+            )}
         </main>
     );
 };
